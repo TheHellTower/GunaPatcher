b6178c5bc346af462dda8033ee90315f12206150
TheHellTower <thehelltower@tuta.io>
2023-09-02 23:54:38 -0400

    Add project files.

---
+++ GunaPatcher/Program.cs
@@ -0,0 +1,51 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using OpCodes = dnlib.DotNet.Emit.OpCodes;
+
+namespace GunapPatcher
+{
+    class Program
+    {
+        private static ModuleDefMD Module = null;
+        private static string NewLocation = string.Empty;
+        static void Main(string[] args)
+        {
+            Module = ModuleDefMD.Load(args[0]);
+
+            NewLocation = Module.Location.Replace(Module.Name, $"Cracked");
+            
+            if (Module.Name != "Guna.UI2" && Module.Assembly.Name != "Guna.UI2")
+            {
+                Console.WriteLine("Are you doing this on purpose ?");
+            }
+            else
+            {
+                Console.WriteLine($"[Loaded] {Module.Assembly.Name} | {Module.Assembly.Version}");
+                foreach (TypeDef Type in Module.Types)
+                    foreach (TypeDef NT in Type.NestedTypes.Where(NT => NT.HasMethods && NT.Methods.Count() == 5 && NT.Fields.Count() == 5))
+                        foreach (MethodDef Method in NT.Methods.Where(M => M.HasBody && M.Body.HasInstructions && M.Body.Instructions.Count() > 300))
+                        {
+                            Method.Body.Variables.Clear();
+                            Method.Body.Instructions.Clear();
+                            Method.Body.ExceptionHandlers.Clear();
+
+                            Method.Body.Instructions.Add(Instruction.Create(OpCodes.Ldstr, "Cracked by TheHellTower"));
+                            Method.Body.Instructions.Add(Instruction.Create(OpCodes.Ldstr, "https://t.me/TheHellTower"));
+                            Method.Body.Instructions.Add(Instruction.Create(OpCodes.Call, Module.Import(typeof(System.Windows.Forms.MessageBox).GetMethod("Show", new Type[] { typeof(string), typeof(string) }))));
+                            Method.Body.Instructions.Add(Instruction.Create(OpCodes.Ret));
+
+                            Console.WriteLine("Cracked !");
+                        }
+            }
+
+            if (!Directory.Exists(NewLocation))
+                Directory.CreateDirectory(NewLocation);
+
+            Module.Write($"{NewLocation}\\{Module.Name}");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,61 @@
+# GunaPatcher
+
+> **Warning** The only official codes/examples are here or in one of the linked repositories if any and nowhere else.
+
+[![CodeFactor](https://www.codefactor.io/repository/github/thehelltower/gunapatcher/badge)](https://www.codefactor.io/repository/github/thehelltower/gunapatcher)
+
+## 📜 What it does ?
+
+This patcher make you able to fully use GunaUI(`Tested on 2.0.4.5`) for free.
+
+<br>
+
+## 🎥 Preview
+
+[![](https://i.imgur.com/isAADUd.gif)](https://i.imgur.com/isAADUd.mp4)
+## 🌟 Stars
+
+- None
+
+## 📚 FAQ
+
+<details>
+    <summary>
+        Will you update this long-term ?
+    </summary>
+    Not sure, depend on Guna I believe.
+</details>
+<details>
+    <summary>
+        Can I support the project ?
+    </summary>
+    Yes, you can either "sponsor" me with the button on my profile or donate by going there: https://github.com/TheHellTower#-support-my-work and read, if you want to donate through PayPal you can add me on Discord, click here to see my Discord: https://github.com/TheHellTower#-socials.
+</details>
+<details>
+    <summary>
+        Can I contribute to the project ?
+    </summary>
+    Yes, feel free to fork it, updated it as you wish as long as you don't break it and open a PR that will be reviewed !
+</details>
+<details>
+    <summary>
+        I need some help, do you give support ?
+    </summary>
+    No.
+</details>
+<details>
+    <summary>
+        Can you make me a custom patcher with everything I need ?
+    </summary>
+    Yes and no, I can't just do that and see a bunch of people in my DMs. However, You can send me an email at: "thehelltower@tuta.io" with your offer(price, etc..)
+
+    Note: Support for the first month included(more if the offer nice) and payment is sent only after preview.
+</details>
+<details>
+    <summary>
+        I have a question, can I contact you ?
+    </summary>
+    Yes you can either by opening a issue: https://github.com/TheHellTower/GunaPatcher/issues/new or send me an email at: "thehelltower@tuta.io" or contact me on one of my socials here: https://github.com/TheHellTower#-socials
+
+    Note: Only for questions no code support.
+</details>
